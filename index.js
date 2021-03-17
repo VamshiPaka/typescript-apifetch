43932b6ee3d8af20893eafb6019063f24123c3a1
VamshiPaka <vamshi.paka@gmail.com>
2021-03-18 00:02:30 +0530

    added interface

--- index.js
+++ index.js
@@ -3,5 +3,9 @@ exports.__esModule = true;
 var axios_1 = require("axios");
 var url = 'https://jsonplaceholder.typicode.com/todos/1';
 axios_1["default"].get(url).then(function (response) {
-    console.log(response.data);
+    var todo = response.data;
+    var ID = todo.id;
+    var title = todo.title;
+    var finished = todo.completed;
+    console.log("The Todo with ID:" + ID + " Has a title of:" + title + " Is it finished: " + finished);
 });
